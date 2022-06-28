969f2fc862d9f850f69d253d81b3e6fffea59cf1
dlobera <delobera@gmail.com>
2022-06-27 22:35:04 -0500

    fix profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 // import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 
 //GET localhost:3000/profiles
-router.get('/', profilesCtrl.index)
+router.get('/', isLoggedIn, profilesCtrl.index)
 
-//
+//GET localhost:3000/profiles/:id
+router.get('/:id', isLoggedIn, profilesCtrl.show)
 
 export{
   router
